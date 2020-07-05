457a7a66739daa430e90749eaa8dac440ef05988
Can221-ParOS <everettbutts@hotmail.com>
2020-07-05 18:42:50 -0400

    Mkpasswd.py added

---
+++ mkpasswd.py
@@ -0,0 +1,21 @@
+        #!/bin/py
+
+#Select a random set of characters
+import random
+
+#Create a list of characters to generate a password from
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*"
+
+#Ask the user how many passwords they want to generate
+number = input("Number of passwords? - ")
+number = int(number)
+
+#Ask the user what length in characters the password should be
+length = input("Password length? - ")
+length = int(length)
+
+for p in range(number):
+    password = " "
+    for c in range(length):
+        password += random.choice(chars)
+    print(password)
